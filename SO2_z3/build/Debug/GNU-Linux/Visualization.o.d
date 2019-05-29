3af37717391d5f39d3869651af7d3dd759bd1c06
Wojciech Gąsiewicz <gasiewicz.wojciech@gmail.com>
2019-05-29 14:52:34 +0200

    sales statistics added
    TODO:
    same in v1.8

---
+++ SO2_z3/build/Debug/GNU-Linux/Visualization.o.d
@@ -0,0 +1,14 @@
+build/Debug/GNU-Linux/Visualization.o: Visualization.cpp Visualization.h \
+ Client.h Distributor.h SalesMan.h CarWash.h GasStation.h
+
+Visualization.h:
+
+Client.h:
+
+Distributor.h:
+
+SalesMan.h:
+
+CarWash.h:
+
+GasStation.h:
