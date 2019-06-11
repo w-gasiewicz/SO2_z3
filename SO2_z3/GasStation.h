d0d9d81d63d7f1539699594fee9be4adfa2edd56
Wojciech Gąsiewicz <gasiewicz.wojciech@gmail.com>
2019-06-11 07:44:06 +0200

    no condition variables

---
+++ SO2_z3/GasStation.h
@@ -0,0 +1,15 @@
+#ifndef GASSTATION_H
+#define GASSTATION_H
+
+class GasStation {
+public:
+    GasStation();
+    virtual ~GasStation();
+    double income;
+    int servicedCustomers;
+private:
+
+};
+
+#endif /* GASSTATION_H */
+
---
+++ SO2_z3/SalesMan.h
@@ -0,0 +1,17 @@
+#ifndef SALESMAN_H
+#define SALESMAN_H
+class SalesMan {
+public:
+    SalesMan();
+    SalesMan(int id, bool isUsed,int xcord,int ycord);
+    void SetBussy(int cID);
+    void SetFree();
+    int id,xcord,ycord,clientID;
+    bool isWorking;
+    virtual ~SalesMan();
+private:
+
+};
+
+#endif /* SALESMAN_H */
+
